a39170a8d9d010af6c46978b55b3409f7c11f128
James Zhou <zihzhou@paypal.com>
2016-11-06 02:32:10 -0800

    😥  pasted some code which apparently does not work...

--- TwiSwift/ProfileViewController.swift
+++ TwiSwift/ProfileViewController.swift
@@ -8,30 +8,175 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIScrollViewDelegate {
+    
+    let offset_HeaderStop:CGFloat = 40.0 // At this offset the Header stops its transformations
+    let distance_W_LabelHeader:CGFloat = 30.0 // The distance between the top of the screen and the top of the White Label
     
     var user: User!
     
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var headerLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var screenNameLabel: UILabel!
+    
+    @IBOutlet weak var segmentedView: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var seperatorLineView: UIView!
+    
+    var headerImageView: UIImageView!
+    var headerBlurImageView: UIImageView!
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.navigationBar.isHidden = true
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.contentInset = UIEdgeInsetsMake(headerView.frame.height, 0, 0, 0)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+
+        avatarImageView.setImageWith(URL(string: "\(user.profileImageUrl!)")!)
+        nameLabel.text = user.name
+        screenNameLabel.text = "@\(user.screenname!)"
+        
+        headerImageView = UIImageView(frame: headerView.bounds)
+        headerImageView.setImageWith(URL(string: User.getDisplayableBannerURL(user: user))!)
+        headerImageView.contentMode = .scaleAspectFill
+        headerView.insertSubview(headerImageView, belowSubview: headerLabel)
+        
+        headerBlurImageView = UIImageView(frame: headerView.bounds)
+        headerBlurImageView.setImageWith(URL(string: User.getDisplayableBannerURL(user: user))!)
+        headerBlurImageView.image? = headerBlurImageView.image!.blurredImage(withRadius: 10, iterations: 20, tintColor: UIColor.clear)
+        headerBlurImageView.contentMode = .scaleAspectFill
+        headerBlurImageView.alpha = 0.0
+        headerView.insertSubview(headerBlurImageView, belowSubview: headerLabel)
+        
+        headerView.clipsToBounds = true
+        
+        seeBorder(view: headerView)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         self.navigationController?.navigationBar.isHidden = false
     }
+    
+    // MARK: - Scroll View Delegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        let offset = scrollView.contentOffset.y + headerView.bounds.height
+        
+        var avatarTransform = CATransform3DIdentity
+        var headerTransform = CATransform3DIdentity
+        
+        if offset < 0 {
+            
+            let headerScaleFactor:CGFloat = -(offset) / headerView.bounds.height
+            let headerSizevariation = ((headerView.bounds.height * (1.0 + headerScaleFactor)) - headerView.bounds.height)/2
+            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
+            headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
+            
+            // Hide views if scrolled super fast
+            headerView.layer.zPosition = 0
+            headerLabel.isHidden = true
+        } else {
+            
+            
+            headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offset_HeaderStop, -offset), 0)
+            
+            //  ------------ Label
+            
+            headerLabel.isHidden = false
+            let alignToNameLabel = -offset + nameLabel.frame.origin.y + headerView.frame.height + offset_HeaderStop
+            
+            headerLabel.frame.origin = CGPoint(x: headerLabel.frame.origin.x, y: max(alignToNameLabel, distance_W_LabelHeader + offset_HeaderStop))
+            
+            headerBlurImageView?.alpha = min (1.0, (offset - alignToNameLabel)/distance_W_LabelHeader)
+            
+            // Avatar -----------
+            
+            let avatarScaleFactor = (min(offset_HeaderStop, offset)) / avatarImageView.bounds.height / 1.4 // Slow down the animation
+            let avatarSizeVariation = ((avatarImageView.bounds.height * (1.0 + avatarScaleFactor)) - avatarImageView.bounds.height) / 2.0
+            avatarTransform = CATransform3DTranslate(avatarTransform, 0, avatarSizeVariation, 0)
+            avatarTransform = CATransform3DScale(avatarTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0)
+            
+            if offset <= offset_HeaderStop {
+                if avatarImageView.layer.zPosition < headerView.layer.zPosition{
+                    headerView.layer.zPosition = 0
+                }
+            } else {
+                if avatarImageView.layer.zPosition >= headerView.layer.zPosition{
+                    headerView.layer.zPosition = 2
+                }
+            }
+        }
+        
+        // Apply Transformations
+        headerView.layer.transform = headerTransform
+        avatarImageView.layer.transform = avatarTransform
+        
+        // Segment control
+        
+        let segmentViewOffset = profileView.frame.height - segmentedView.frame.height - offset
+        
+        var segmentTransform = CATransform3DIdentity
+        
+        // Scroll the segment view until its offset reaches the same offset at which the header stopped shrinking
+        segmentTransform = CATransform3DTranslate(segmentTransform, 0, max(segmentViewOffset, -offset_HeaderStop), 0)
+        
+        segmentedView.layer.transform = segmentTransform
+        
+        
+        // Set scroll view insets just underneath the segment control
+        tableView.scrollIndicatorInsets = UIEdgeInsetsMake(segmentedView.frame.maxY, 0, 0, 0)
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    // MARK: - Table View
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Tweet Tweet!"
+        return cell
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func seeBorder(view: UIView) {
+        view.layer.borderColor = UIColor.red.cgColor
+        view.layer.borderWidth = 3
     }
 }
